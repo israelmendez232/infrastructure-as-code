b8945b3526f28ef9c21e64f2f6e78657f7493889
Israel Mendes <israel.mendez232@gmail.com>
2020-05-25 06:02:54 -0300

    Auto Scalling and ELB

--- prod.tf
+++ prod.tf
@@ -5,6 +5,14 @@ provider "aws" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+    availability_zone = "us-west-2a"
+}
+
+resource "aws_default_subnet" "default_az2" {
+    availability_zone = "us-west-2b"
+}
+
 resource "aws_security_group" "prd" {
     name = "prd_server"
     description = "Allow the API requests for the server."
@@ -35,22 +43,6 @@ resource "aws_security_group" "prd" {
     }
 }
 
-
-resource "aws_instance" "prd" {
-    count = 2
-
-    ami = "ami-03c8adc67e56c7f1d"
-    instance_type = "t2.nano"
-
-    vpc_security_group_ids = [
-        aws_security_group.prd.id
-    ]
-
-    tags = {
-        "Terraform": "true"
-    }
-}
-
 resource "aws_eip_association" "prd" {
     instance_id = aws_instance.prd[0].id
     allocation_id = aws_eip.prd.id
@@ -62,4 +54,51 @@ resource "aws_eip" "prd" {
     tags = {
         "Terraform": "true"
     }
+}
+
+resource "aws_elb" "prd_web" {
+    name = "prd-web"
+    instances = aws_instance.prd.*.id
+    subnets = ["aws_default_subnet.default_az1.id", "aws_default_subnet.default_az2"]
+    security_groups = [aws_security_group.prd.id]
+
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+}
+
+resource "aws_launch_template" "prd" {
+    name_prefix = "prd-web"
+    image_id = "ami-03c8adc67e56c7f1d"
+    instance_type = "t2.micro"
+    tags = {
+        "Terraform": "true"
+    }
+}
+
+resource "aws_autoscaling_group" "prd" {
+    availability_zones = ["us-east-1a", "us-east-2b"]
+    vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+    desired_capacity = 1
+    max_size = 1
+    min_size = 1
+
+    launch_template {
+        id = aws_launch_template.prd.id
+        version = "$latest"
+    }
+
+    tag {
+        key = "Terraform"
+        value = "true"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_autoscaling_attachment" "prd" {
+    autoscaling_group_name = aws_autoscaling_group.prd.id
+    elb = aws_elb.prd_web.id
 }
